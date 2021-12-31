e33c8d00168cf42928b93b0416e754c8244b3ace
Abose Ukhun <bexis17@gmail.com>
2021-12-31 14:20:36 +0300

    add has_many reviews relationship to dormitory, add average score and slugify the name

--- app/models/dormitory.rb
+++ app/models/dormitory.rb
@@ -1,2 +1,12 @@
 class Dormitory < ApplicationRecord
+    has_many :reviews
+
+    before_create :doSlugging
+
+    def doSlugging
+        self.slug = name.parameterize
+    end
+
+    def average_score
+        reviews.average(:score).round(2).to_f
 end
